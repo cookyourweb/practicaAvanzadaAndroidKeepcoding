f4329679280ed0ce64d2d2b266b5cef25d0548f2
cookyourweb <hello.cookyourweb@gmail.com>
2023-05-28 20:09:32 +0200

    cambio

--- practicaAndroidAvanzadoKeepcoding/app/src/main/java/com/example/segundapracticaandroidfundamentos/model/Heroe.kt
+++
@@ -1,8 +0,0 @@
-package com.example.practicafundamentosandroid.model
-
-data class Heroe(
-    val name : String,
-    val photo: String,
-    val vidaTotal: Int,
-    var vidaRestante: Int
-)
--- practicaAndroidAvanzadoKeepcoding/app/src/main/java/com/example/segundapracticaandroidfundamentos/model/HeroeDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.example.practicafundamentosandroid.model
-
-data class HeroeDTO(
-    val description: String,
-    val name : String,
-    val id: String,
-    val favorite: Boolean,
-    val photo: String
-)
